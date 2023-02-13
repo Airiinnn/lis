0342f017adb32f9cc170fb21a227ede15f13dee3
Alexander Liswandy <alex.liswandy@hotmail.com>
2023-02-13 17:32:48 +0800

    Accomodated smaller computer screens. Need to make mobile friendly

--- src/components/Achievements.js
+++ src/components/Achievements.js
@@ -20,43 +20,45 @@ export default function Achievements() {
 
     return (
         <section id="achievements" ref={ref}>
-            <div className={`${inView ? "achievements-1" : "achievements-0"}`}>
-                <div className="section-header">
-                    <h1 className="center">Achievements</h1>
-                    <span>               
-                        <img className="section-cube" src={iconCube} alt="cube" />
-                        <img className="section-cube" src={iconCube} alt="cube" />
-                        <img className="section-cube" src={iconCube} alt="cube" />
-                    </span>
-                    <p className="p3 center">Here are the achievements I have attained and the competitions I have participated in.</p>
-                </div>
+            <div className="content-area">
+                <div className={`${inView ? "achievements-1" : "achievements-0"}`}>
+                    <div className="section-header">
+                        <h1 className="center">Achievements</h1>
+                        <span>               
+                            <img className="section-cube" src={iconCube} alt="cube" />
+                            <img className="section-cube" src={iconCube} alt="cube" />
+                            <img className="section-cube" src={iconCube} alt="cube" />
+                        </span>
+                        <p className="p3 center">Here are the achievements I have attained and the competitions I have participated in.</p>
+                    </div>
 
-                <div id="achievements-list">
-                    <Swiper
-                        style={{
-                            "--swiper-navigation-color": "#f1beb0",
-                            "--swiper-navigation-size": "25px",
-                        }}
-                        modules={[Navigation, Autoplay]}
-                        slidesPerView={3}
-                        navigation={true}
-                        loop={true}
-                        speed={1000} // 1s transition time
-                        autoplay={{
-                            delay: 3000, // stays for 3s
-                            disableOnInteraction: false,
-                        }}
-                        effect={"fade"}
-                    >   
-                        {achievements.map((achievement) => (
-                            <SwiperSlide>
-                                <div className="achievement-card">
-                                    <p className="p2">{achievement.name}</p>
-                                    <p>{achievement.award}</p>
-                                </div>
-                            </SwiperSlide>
-                        ))}
-                    </Swiper>
+                    <div id="achievements-list">
+                        <Swiper
+                            style={{
+                                "--swiper-navigation-color": "#f1beb0",
+                                "--swiper-navigation-size": "25px",
+                            }}
+                            modules={[Navigation, Autoplay]}
+                            slidesPerView={3}
+                            navigation={true}
+                            loop={true}
+                            speed={1000} // 1s transition time
+                            autoplay={{
+                                delay: 3000, // stays for 3s
+                                disableOnInteraction: false,
+                            }}
+                            effect={"fade"}
+                        >   
+                            {achievements.map((achievement) => (
+                                <SwiperSlide>
+                                    <div className="achievement-card">
+                                        <p className="p2">{achievement.name}</p>
+                                        <p>{achievement.award}</p>
+                                    </div>
+                                </SwiperSlide>
+                            ))}
+                        </Swiper>
+                    </div>
                 </div>
             </div>
         </section>
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -14,39 +14,41 @@ export default function Projects() {
 
     return (
         <section id="projects" ref={ref}>
-            <div className={`section-header ${inView ? "projects-header-1" : "projects-header-0"}`}>
-                <h1>Projects</h1>
-                <span>
-                    <img className="section-cube" src={iconCube} alt="cube" />
-                    <img className="section-cube" src={iconCube} alt="cube" />
-                </span>
-                <p className="p3 center">These are some of featured projects that I have worked on.</p>
-            </div>
-            
-            <div className="project-list">
-                {projectsFeatured.map((project, index) => (
-                    <ProjectFeatured
-                        key={project.title}
-                        index={index}
-                        title={project.title}
-                        subtitle={project.subtitle}
-                        website={project.website}
-                        github={project.github}
-                        description={project.description}
-                        tech={project.tech}
-                        img={project.img}
-                        type={project.type}
-                    />
-                ))}
-            </div>
-            
-            <div className={`view-more ${inView ? "projects-header-1" : "projects-header-0"}`}>
-                <a className="view-more-button-a" href="#projects" >
-                    <div className="view-more-button">
-                        <p className="p3">View All (soon)</p>
-                    </div>
-                </a>
-            </div>            
+            <div className="content-area">
+                <div className={`section-header ${inView ? "projects-header-1" : "projects-header-0"}`}>
+                    <h1>Projects</h1>
+                    <span>
+                        <img className="section-cube" src={iconCube} alt="cube" />
+                        <img className="section-cube" src={iconCube} alt="cube" />
+                    </span>
+                    <p className="p3 center">These are some of featured projects that I have worked on.</p>
+                </div>
+                
+                <div className="project-list">
+                    {projectsFeatured.map((project, index) => (
+                        <ProjectFeatured
+                            key={project.title}
+                            index={index}
+                            title={project.title}
+                            subtitle={project.subtitle}
+                            website={project.website}
+                            github={project.github}
+                            description={project.description}
+                            tech={project.tech}
+                            img={project.img}
+                            type={project.type}
+                        />
+                    ))}
+                </div>
+                
+                <div className={`view-more ${inView ? "projects-header-1" : "projects-header-0"}`}>
+                    <a className="view-more-button-a" href="#projects" >
+                        <div className="view-more-button">
+                            <p className="p3">View All (soon)</p>
+                        </div>
+                    </a>
+                </div>
+            </div>       
         </section>
     );
 }
