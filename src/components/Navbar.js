6b45059c1094b9d4548db9039388f12a2150a6e1
Alexander Liswandy <alex.liswandy@hotmail.com>
2023-02-14 16:04:08 +0800

    Added mobile navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,16 @@
-import { useEffect } from "react";
+import { useState, useEffect } from "react";
 
 import "../assets/styles/navbar.css"
 
 import iconCube from "../assets/images/icons/cube-pink.png"
 
 export default function Navbar() {
+    const [mobileMenuActive, setMobileMenuActive] = useState(false);
+
+    const toggleMobileMenu = () => {
+        setMobileMenuActive(current => !current);
+    }
+
     useEffect(() => {
         var prevScrollpos = window.pageYOffset;
         window.onscroll = function() {
@@ -22,7 +28,7 @@ export default function Navbar() {
     return (
         <header id="navbar">
             <div className="nav-left">
-                <a href="/">
+                <a href="#about">
                     <div className="nav-icon">
                         <img src={iconCube} alt="cube" className="icon" />
                         <span className="nav-text bold">lis</span>
@@ -30,14 +36,14 @@ export default function Navbar() {
                 </a>
             </div>
             
-            <div className="nav-middle">
+            <nav className="nav-middle">
                 <ul className="nav-links">
                     <li className="nav-link"><a href="#aboutme">About Me</a></li>
                     <li className="nav-link"><a href="#projects">Projects</a></li>
                     <li className="nav-link"><a href="#achievements">Achievements</a></li>
                     <li className="nav-link"><a href="#contact">Contact</a></li>
                 </ul>
-            </div>
+            </nav>
 
             <div className="nav-right">
                 <div className="resume">
@@ -47,6 +53,27 @@ export default function Navbar() {
                         </div>
                     </a>
                 </div>
+            </div>
+
+            <div id="hamburger-icon" className={`${mobileMenuActive ? "open": "closed"}`} onClick={toggleMobileMenu}>
+                <div className="bar bar-1"></div>
+                <div className="bar bar-2"></div>
+                <div className="bar bar-3"></div>
+
+                <ul className={`mobile-menu ${mobileMenuActive ? "nav-list-1": "nav-list-0"}`}>
+                    <li><a href="#aboutme">About Me</a></li>
+                    <li><a href="#projects">Projects</a></li>
+                    <li><a href="#achievements">Achievements</a></li>
+                    <li><a href="#contact">Contact</a></li>
+
+                    <li className="resume">
+                        <a className="resume-button-a" href="resume.pdf" >
+                            <div className="resume-button">
+                                <p>Resume</p>
+                            </div>
+                        </a>
+                    </li>
+                </ul>
             </div>
         </header>
     );
